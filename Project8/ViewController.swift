c5e420bab20cc8a30d33955a7acbd9152f0c8e72
yashtamakuwala <neel64tamakuwala@gmail.com>
2015-11-06 22:49:02 +0530

    Initial Commit

---
+++ Project8/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by YASH on 05/11/15.
+//  Copyright © 2015 YASH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    
+    var score = 0
+    var level = 1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        for subview in view.subviews{
+            if subview.tag == 10{
+                let btn = subview as! UIButton
+                letterButtons.append(btn)
+                
+                btn.addTarget(self, action: "letterTapped:", forControlEvents: .TouchUpInside) //adding action to the button
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func submitTapped(sender: AnyObject) {
+    }
+   
+    @IBAction func clearTapped(sender: AnyObject) {
+    }
+
+}
+
